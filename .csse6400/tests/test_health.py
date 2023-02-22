9ff1387536098207079f152502cd9302857cd119
bearli-k <65012080+bearli-k@users.noreply.github.com>
2023-02-22 01:29:31 +0000

    health endpoint

--- .csse6400/tests/test_health.py
+++ .csse6400/tests/test_health.py
@@ -4,7 +4,7 @@ from todo import create_app
 class TestHealth(unittest.TestCase):
     def setUp(self):
         self.client = create_app().test_client()
-
+pip
     def test_health(self):
         response = self.client.get('/api/v1/health')
         self.assertEqual(response.status_code, 200)
